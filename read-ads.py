7d2a2ed014a0f9754552d19937a3138758eab61b
ents <entsupml@gmail.com>
2014-11-23 05:12:33 -0500

    added parameters for dir and device id

--- read-ads.py
+++ read-ads.py
@@ -1,8 +1,17 @@
 import urllib2
+import sys
 import json
 import pprint
 
-deviceId="32"
+
+pprint.pprint(sys.argv)
+
+deviceId=sys.argv[1]
+outputDir=(sys.argv[2])+"/"
+
+print "Device ID: " + deviceId
+print "dir: " + outputDir
+
 url = "http://ad-line.biz/json/getAds?device_id="+deviceId
 #print url
 text=urllib2.urlopen(url).read(1000)
@@ -10,7 +19,7 @@ print "JSON received:"
 print text
 data=json.loads(text)
 for item in data:
-    filename=item["ad_id"]+".txt";
+    filename=outputDir + item["ad_id"]+".txt";
     print(filename+": "+item["ad"])
     f = open(filename, "w");
     f.write(item["ad"])
